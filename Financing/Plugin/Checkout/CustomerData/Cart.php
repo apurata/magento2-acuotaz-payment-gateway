262bb9c74ee8aedd4d245509f6097692acdb46e9
Rodrigo Pulcha <rodrigo@apurata.com>
2020-07-23 19:39:57 -0500

    Remove large unnecessary comment

--- Financing/Plugin/Checkout/CustomerData/Cart.php
+++ Financing/Plugin/Checkout/CustomerData/Cart.php
@@ -20,19 +20,6 @@ class Cart {
     public function afterGetSectionData(\Magento\Checkout\CustomerData\Cart $subject, array $result)
     {
         $addOn = $this->getAddOn($result['subtotalAmount']);
-        /*if (floatval($result['subtotalAmount']) >= 100 && floatval($result['subtotalAmount']) <= 600){
-            $all_pos = null;
-            $all_pos = $this->get_all_pos($result['subtotalAmount']);
-            $min_step = floatval($result['subtotalAmount']);
-            if ($all_pos) {
-                foreach ($all_pos as $pos) {
-                    if ($pos->amount == $result['subtotalAmount'] && $pos->payment_igv < $min_step) {
-                        $min_step = $pos->payment_igv;
-                    }
-                }
-            }
-            $extra_data = 'Pagalo en cuotas desde S/'.$min_step;
-        }*/
         $result['extra_data'] = $addOn;
         return $result;
     }
