0f1a84f19aeb111ace1e407d17d6364ca997f32f
ZzzandyzzZ <andy_gean@hotmail.com>
2024-05-07 07:52:31 -0500

    Refact Intent class

--- Apurata/Financing/Controller/Order/Intent.php
+++ Apurata/Financing/Controller/Order/Intent.php
@@ -2,12 +2,14 @@
 
 namespace Apurata\Financing\Controller\Order;
 
-use Psr\Log\LoggerInterface;
+use Apurata\Financing\Helper\ConfigData;
+use Apurata\Financing\Helper\RequestBuilder;
+use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\UrlInterface;
-use Apurata\Financing\Helper\ConfigData;
+use Psr\Log\LoggerInterface;
 
 
 class Intent extends Action
@@ -17,6 +19,7 @@ class Intent extends Action
         private LoggerInterface $logger,
         private CheckoutSession $checkoutSession,
         private UrlInterface $urlBuilder,
+        private RequestBuilder $requestBuilder,
         private \Magento\Customer\Model\Session $customerSession2
     ) {
         return parent::__construct($context);
@@ -25,44 +28,64 @@ class Intent extends Action
     public function execute()
     {
         $order = $this->checkoutSession->getLastRealOrder();
+        $this->handleApurataId();
+        $intentParams = $this->buildIntentParams($order);
+        $this->checkoutSession->restoreQuote();
+        list($respCode, $response) = $this->requestBuilder->makeCurlToApurata('POST', ConfigData::APURATA_CREATE_ORDER_URL, $intentParams);
+        $response = json_decode($response);
+        if ($respCode == 200) {
+            $this->_redirect($response->redirect_to);
+        } else {
+            error_log(sprintf('Apurata log: Error al crear orden http_code %s', $respCode));
+            $objectManager = ObjectManager::getInstance();
+            $messageManager = $objectManager->get('Magento\Framework\Message\ManagerInterface');
+            $messageManager->addError('Hubo un error al procesar el pago con aCuotaz. Por favor, inténtelo de nuevo más tarde.');
+            $this->_redirect($this->urlBuilder->getBaseUrl());
+        }
+    }
+
+    private function handleApurataId()
+    {
         try {
             if (!$this->customerSession2->getApurataId()) {
                 $this->customerSession2->setApurataId($this->customerSession2->getSessionId());
             }
         } catch (\Throwable $e) {
-            error_log('Error:can not get session_id');
+            error_log('Apurata log: cannot get session_id');
         }
-        $session_id = $this->customerSession2->getApurataId();
-        $intentParams = '?pos_client_id=' . $this->getRequest()->getParam('pos_client_id') .
-            '&order_id=' . urlencode($order->getId()) .
-            '&amount=' . urlencode($order->getGrandTotal()) .
-            '&url_redir_on_canceled=' . urlencode(rtrim($this->urlBuilder->getUrl(ConfigData::FINANCING_FAIL_URL), '/')) .
-            '&url_redir_on_rejected=' . urlencode(rtrim($this->urlBuilder->getUrl(ConfigData::FINANCING_FAIL_URL), '/')) .
-            '&url_redir_on_success=' . urlencode(rtrim($this->urlBuilder->getUrl(ConfigData::FINANCING_SUCCESS_URL . $order->getId()), '/'));
-        $billing_address = $order->getBillingAddress();
-        if ($billing_address) {
-            $intentParams .= '&customer_data__email=' . urlencode($billing_address->getData('email')) .
-                '&customer_data__phone=' . urlencode($billing_address->getData('telephone')) .
-                '&customer_data__billing_address_1=' . urlencode($billing_address->getData('street')) .
-                '&customer_data__billing_first_name=' . urlencode($billing_address->getData('firstname')) .
-                '&customer_data__billing_last_name=' . urlencode($billing_address->getData('lastname')) .
-                '&customer_data__billing_city=' . urlencode($billing_address->getData('city'));
-        }
-        $shipping_address = $order->getShippingAddress();
-        if ($shipping_address) {
-            $intentParams .= '&customer_data__shipping_address_1=' . urlencode($shipping_address->getData('street')) .
-                '&customer_data__shipping_first_name=' . urlencode($shipping_address->getData('firstname')) .
-                '&customer_data__shipping_last_name=' . urlencode($shipping_address->getData('lastname')) .
-                '&customer_data__shipping_city=' . urlencode($shipping_address->getData('city')) .
-                '&customer_data__dni=' . urlencode($this->get_dni_field_id($order));
-        }
-        $intentParams .= '&customer_data__session_id=' . urldecode($session_id);
-
-
-        $this->checkoutSession->restoreQuote();
-        $this->_redirect(ConfigData::APURATA_DOMAIN . ConfigData::APURATA_CREATE_ORDER_URL . $intentParams);
     }
-    public function get_dni_field_id($order)
+
+    private function buildIntentParams($order)
+    {
+        $billingAddress = $order->getBillingAddress();
+        $shippingAddress = $order->getShippingAddress();
+        $customerData = [
+            'email' => $billingAddress->getData('email'),
+            'phone' => $billingAddress->getData('telephone'),
+            'billing_address_1' => $billingAddress->getData('street'),
+            'billing_first_name' => $billingAddress->getData('firstname'),
+            'billing_last_name' => $billingAddress->getData('lastname'),
+            'billing_city' => $billingAddress->getData('city'),
+            'shipping_address_1' => $shippingAddress->getData('street'),
+            'shipping_first_name' => $shippingAddress->getData('firstname'),
+            'shipping_last_name' => $shippingAddress->getData('lastname'),
+            'shipping_city' => $shippingAddress->getData('city'),
+            'dni' => $this->getDniFieldId($order),
+            'session_id' => $this->customerSession2->getApurataId(),
+        ];
+        $intentParams = [
+            'pos_client_id' => $this->getRequest()->getParam('pos_client_id'),
+            'order_id' => $order->getId(),
+            'amount' => $order->getGrandTotal(),
+            'url_redir_on_canceled' => rtrim($this->urlBuilder->getUrl(ConfigData::FINANCING_FAIL_URL), '/'),
+            'url_redir_on_rejected' => rtrim($this->urlBuilder->getUrl(ConfigData::FINANCING_FAIL_URL), '/'),
+            'url_redir_on_success' => rtrim($this->urlBuilder->getUrl(ConfigData::FINANCING_SUCCESS_URL . $order->getId()), '/'),
+            'customer_data' => $customerData,
+        ];
+        return $intentParams;
+    }
+
+    private function getDniFieldId($order)
     {
         $dni = $order->getBillingAddress()->getData('dni') ??
             $order->getBillingAddress()->getData('DNI') ??
